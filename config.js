00ec3babc6c5abe4dbf9a7486420799c20703c86
Zenng33333 <85278597+Zenng33333@users.noreply.github.com>
2021-06-03 14:21:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'MÀY à!',
+    introDesc: `dù qua 1/6 hay còn gọi là
+    quốc tế thiếu nhi á ,mày có muốn tặng 
+    quà không hãy trả lời cho tao biết`,
+    btnIntro: 'OK',
+    title: 'Phải chăng mày không muốn quà 😙',
+    desc: 'Phải chăng em mày đã trưởng thành ',
+    btnYes: 't trưởng thành rồi t không cần quà nữa <33',
+    btnNo: 't vẫn cần quà:3',
+    question:'Trên thế giới hơn 7 tỉ người mà mày lại đòi quà t <3',
+    btnReply: 'Gửi cho t <3',
+    reply: 't khoong thích quà đâu t trưởng thành rồi <33333333',
+    mess: 't biết mà 🥰. cảm ơn mày nhiều nhiều 😘😘',
+    messDesc: 'không cần quà đúng không.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.messenger.com/t/100057340044692' 
 }
